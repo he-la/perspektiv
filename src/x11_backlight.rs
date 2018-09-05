dc8b2a00e71c91a2cf56e14e0e0140605c98b61f
Henrik Laxhuber <henrik@laxhuber.com>
2018-09-05 14:50:54 +0200

    Added License
    perspektiv is now licensed under GPLv3, having before had no license
    at all.

--- src/x11_backlight.rs
+++ src/x11_backlight.rs
@@ -1,3 +1,18 @@
+// This file is part of perspektiv, a userspace daemon for graphically reporting
+// system events.
+// Copyright © 2018  Henrik Laxhuber <henrik@laxhuber.com>
+//
+// perspektiv is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License, version 3, as published by the
+// Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
 // BIG TODO:
 // Switch to xcb-rs
 extern crate x11;
